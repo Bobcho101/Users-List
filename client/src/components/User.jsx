0642ca4e6f1d7dd4a49bfd569bff7f3a59d74a3f
Bobcho101 <bgbobi878@gmail.com>
2025-03-01 13:04:32 +0200

    rendering the data

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,19 +1,29 @@
-export default function User(){
+export default function User(user){
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const isoString = date.toISOString();
+        const result = isoString.split('T')[0]
+        return result;
+    }
+
+    const createdAt = formatDate(user.createdAt);
+
+  
     return(
         <>
             <tr>
               <td>
                 <img
-                  src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+                  src={user.imageUrl}
                   alt="Peter's profile"
                   className="image"
                 />
               </td>
-              <td>Peter</td>
-              <td>Johnson</td>
-              <td>peter@abv.bg</td>
-              <td>0812345678</td>
-              <td>June 28, 2022</td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+              <td>{user.phoneNumber}</td>
+              <td>{createdAt}</td>
               <td className="actions">
                 <button className="btn edit-btn" title="Edit">
                   <svg
